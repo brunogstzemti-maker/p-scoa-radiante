5374d76a47ea4c66dc680d7c57e5f1cc8f6ffc49
Bruno Campos <brunogstz.emti@gmail.com>
2026-03-04 19:40:42 -0300

    style: remove specific image testimonial

--- src/components/TestimonialsSection.tsx
+++ src/components/TestimonialsSection.tsx
@@ -1,7 +1,7 @@
 import AnimateOnScroll from "./AnimateOnScroll";
 import testImg1 from "@/assets/testimonial-img-1.jpg";
 import testImg2 from "@/assets/testimonial-img-2.jpg";
-import testImg3 from "@/assets/testimonial-img-3.jpg";
+
 import testImg4 from "@/assets/testimonial-img-4.jpg";
 import testImg5 from "@/assets/testimonial-img-5.jpg";
 import testImg6 from "@/assets/testimonial-img-6.jpg";
@@ -21,7 +21,7 @@ const TestimonialsSection = () => {
         </AnimateOnScroll>
 
         <div className="grid grid-cols-1 gap-6 max-w-xl mx-auto">
-          {[testImg1, testImg2, testImg3, testImg4, testImg5, testImg6, testImg7, testImg8].map((img, i) => (
+          {[testImg1, testImg2, testImg4, testImg5, testImg6, testImg7, testImg8].map((img, i) => (
             <AnimateOnScroll key={i} delay={200 + i * 100}>
               <div className="rounded-2xl overflow-hidden shadow-md border border-border">
                 <img src={img} alt={`Depoimento ${i + 1}`} className="w-full h-auto object-contain" loading="lazy" />
